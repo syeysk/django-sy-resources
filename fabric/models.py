182016e9e57e5042aa30ec6c7b47eaf75c0d6c37
shyzik93 <shyzik93@mail.ru>
2023-11-16 02:26:40 +0300

    change fabric model

--- fabric/models.py
+++ fabric/models.py
@@ -3,5 +3,10 @@ from django.db import models
 
 
 class Fabric(models.Model):
-    title = models.CharField('Название фабрики', max_length=240)
+    title = models.CharField('Название фабрики', max_length=240, unique=True)
     user = models.ForeignKey(get_user_model(), null=False, on_delete=models.CASCADE)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=('title',), name='index_fabric'),
+        ]
