ee3077de742ab976058595d3f2f849baf45a92ec
shyzik93 <shyzik93@mail.ru>
2024-01-08 04:05:43 +0300

    fix viewing image

--- static/resource/view-images-component.js
+++ static/resource/view-images-component.js
@@ -2,7 +2,7 @@ ViewImagesComponent = {
     props: ['value'],
     template: `
         <div style="text-align: center;">
-            <img v-for="image in value" style="width: 100px; margin-left: 15px;" :src="image.image" :key="image.pk">
+            <img v-for="image in value" style="width: 100px; margin-right: 15px;" :src="image.image" :key="image.pk">
         </div>
         <slot/>
     `,
